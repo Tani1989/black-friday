4f55e466213114cd79bca6d0a0eab9de87d4abb9
tanika1989 <tanika.sh@gmail.com>
2018-11-29 23:52:49 -0600

    barplot

--- p1.R
+++ p1.R
@@ -7,3 +7,17 @@ mainData <- read.csv("BlackFriday.csv",header = TRUE)
 head(mainData)
 
 # 1) Which gender made the maximum purchase?
+colnames(mainData)
+subset_Data1 <- mainData[,c(3,12)]
+head(subset_Data1)
+
+# Aggregate the data according to the gender.
+
+Aggregate_data <- aggregate(as.numeric(subset_Data1$Purchase) ~ Gender,data = subset_Data1,FUN = sum)
+
+# Rename Column name
+names(Aggregate_data)[2] <- paste("Purchase")
+
+Aggregate_data
+
+# creating bar graph
